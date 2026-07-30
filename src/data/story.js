08748d63497dc12441dafcd4d70eddde139be3d0
SUNIL <101034663+SUNIL-BALRAJ@users.noreply.github.com>
2026-07-30 13:07:49 +0530

    Create story.js with narrative images and text
    Added a story array with images and text for a narrative.

---
+++ src/data/story.js
@@ -0,0 +1,28 @@
+import img1 from "../assets/images/img1.jpg";
+import img2 from "../assets/images/img2.jpg";
+import img3 from "../assets/images/img3.jpg";
+import img4 from "../assets/images/img4.jpg";
+import img5 from "../assets/images/img5.jpg";
+
+export const story = [
+  {
+    image: img1,
+    text: "Hiiii kaadhu ma, Just wanted to tell you somethingggg...",
+  },
+  {
+    image: img2,
+    text: "Do you remember this picture? Our first picture that we took ✨",
+  },
+  {
+    image: img3,
+    text: "Our first official date... 🥺",
+  },
+  {
+    image: img4,
+    text: "A day full of enjoyymenttt 😌",
+  },
+  {
+    image: img5,
+    text: "Yeah, sometimes I do things that make you angry, and I am sorry for that. But just like this image, I choose US. So manichi yethukko kaadhu ma ❤️",
+  },
+];
